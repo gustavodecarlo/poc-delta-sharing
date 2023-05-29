2f42e204c9ce5bd2fbcf8e5b4138323cbf57db90
Gustavo Decarlo <gustavodecarlo@gmail.com>
2023-05-29 17:14:27 -0300

    Feat/azure setup (#1)
    * feat: add azure setup
    
    * fix: azure yml to config delta sharing server

--- start_ds_server_gcp.sh
+++ start_ds_server_gcp.sh
@@ -4,4 +4,4 @@ docker run -p 8889:8080 \
 --mount type=bind,source=<complete path to>/delta_sharing_server.yml,target=/config/delta-sharing-server-config.yaml \
 --mount type=bind,source=<complete path to google service account key>.json,target=/config/key.json \
 --env GOOGLE_APPLICATION_CREDENTIALS="/config/key.json" \
-deltaio/delta-sharing-server:0.4.0 --config /config/delta-sharing-server-config.yaml
+deltaio/delta-sharing-server:0.6.3 --config /config/delta-sharing-server-config.yaml
