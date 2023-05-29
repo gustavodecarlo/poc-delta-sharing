6bc0545ba4d4690c218bd6617e096be27dfe9075
Gustavo Decarlo <gustavodecarlo@gmail.com>
2023-05-29 17:13:50 -0300

    fix: azure yml to config delta sharing server

--- start_ds_server_azure.sh
+++ start_ds_server_azure.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker run -p 8889:8080 \
---mount type=bind,source=<complete path to>/delta_sharing_server.yml,target=/config/delta-sharing-server-config.yaml \
+--mount type=bind,source=<complete path to>/delta_sharing_server_azure.yml,target=/config/delta-sharing-server-config.yaml \
 --mount type=bind,source=<complete path to core-site.xml with azure credentials>,target=/opt/docker/conf/core-site.xml
 deltaio/delta-sharing-server:0.6.3 --config /config/delta-sharing-server-config.yaml
